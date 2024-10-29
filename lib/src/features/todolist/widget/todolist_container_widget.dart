46f1cae37f2802f41213d9efb1c628bee7103fac
leesunmin1231 <polarlsm1231@gmail.com>
2024-10-29 15:31:51 +0900

    refactor: todolist container change to stateless widget

--- lib/src/features/todolist/widget/todolist_container_widget.dart
+++ lib/src/features/todolist/widget/todolist_container_widget.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:to_do_list/src/features/todolist/widget/todolist_input_widget.dart';
 import 'package:to_do_list/src/features/todolist/widget/todolist_widget.dart';
 
-class TodoListContainer extends ConsumerStatefulWidget {
+class TodoListContainer extends StatelessWidget {
   const TodoListContainer({super.key});
 
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() {
-    return _TodoListContainerState();
-  }
-}
-
-class _TodoListContainerState extends ConsumerState<TodoListContainer> {
   @override
   Widget build(BuildContext context) {
     return Container(
